6b32a39b51e92ed3589c463948611934263e0932
Ellen Marie Dash <me@duckie.co>
2025-11-09 03:12:58 -0500

    [truncate] Remove weird CTE section that doesn't seem to affect anything.

--- src/pg_query_summary_truncate.c
+++ src/pg_query_summary_truncate.c
@@ -482,19 +482,8 @@ static PgQueryError *apply_truncations(Summary *summary, Node *tree, TruncationS
 			printf("Rule/whereClause\n");
 		}
 		else if (IsA(node, CommonTableExpr) && attr == TRUNCATION_CTE_QUERY) {
-			// Equivalent to: https://github.com/pganalyze/pg_query.rs/blob/66eb7becea1a40e315fee3f90197a35a89d20c25/src/truncate.rs#L230-L237
 			CommonTableExpr *stmt = castNode(CommonTableExpr, node);
-			Node *old = stmt->ctequery;
 			stmt->ctequery = dummy_select(NULL, (Node *) dummy_column(), NULL);
-			if (old) {
-				for (size_t i = 0; i < list_length(state->truncations);) {
-					PossibleTruncation *t = (PossibleTruncation *) list_nth_cell(state->truncations, i);
-					if (t->node == old)
-						state->truncations = list_delete_nth_cell(state->truncations, i);
-					else
-						i++;
-				}
-			}
 			printf("CTE/cteQuery\n");
 		}
 		else if (IsA(node, InferClause) && attr == TRUNCATION_WHERE_CLAUSE) {
